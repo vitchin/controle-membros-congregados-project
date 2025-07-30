be240da5ab41839f5d297e71d8c0d8f591456ea2
Victor Ricardo Oliveira Nunes <101266128+vitchin@users.noreply.github.com>
2025-07-30 09:13:55 -0300

    [feat] page.tsx | página de login

---
+++ iebi-membership-app/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Title } from "../components/titulo";
+import { Login } from "../components/login";
+import { Footer } from "../components/footer";
+
+export default function LoginPage() {
+  return (
+    <section className="relative bg-gray-50 min-h-screen flex flex-col items-center align justify-center">
+      <Title />
+      <Login />
+      <Footer />
+    </section>
+  )
+}
