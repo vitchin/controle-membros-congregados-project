107bc680151d1fcce8b60b0f933c67c611169757
Victor Ricardo Oliveira Nunes <victor12092004@gmail.com>
2025-08-01 14:50:47 -0300

    [feat] register.txs | implementa componentes reutilizaveis

--- iebi-membership-app/src/app/components/register.tsx
+++ iebi-membership-app/src/app/components/register.tsx
@@ -1,12 +1,9 @@
-import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox"
-import { Label } from "@/components/ui/label";
-import {
-  Select, SelectContent, SelectItem, SelectTrigger, SelectValue,
-} from "@/components/ui/select";
+import { FormInput } from "./componentInput";
+import { FormSelect } from "./componentSelect";
+import { FormCheckbox } from "./componentCheckbox";
 
-const inputstyle = "mb-6 w-full grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-start";
+const inputstyle = "mb-6 w-full grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-center justify-start";
 
 export function Register() {
   return (
@@ -14,117 +11,91 @@ export function Register() {
       <h2 className="text-center text-2xl font-bold mb-4">REGISTRAR</h2>
       <p className="mb-4 text-center text-gray-500">Informe seus dados pessoais, familiares e ministeriais para completar o registro.</p>
       <form>
-        <h3 className="mb-2 font-semibold">DADOS PESSOAIS</h3>
+        <h3 className="mb-2 font-bold">DADOS PESSOAIS</h3>
         <div className={inputstyle}>
-          <Input placeholder="Nome" required />
-          <Select>
-                <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Sexo" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value="masculino">Masculino</SelectItem>
-                    <SelectItem value="feminino">Feminino</SelectItem>
-                </SelectContent>
-            </Select>
-          <Input type="date" placeholder="Data de Nascimento" required />
-          <Select>
-                <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Estado civil" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value="solteiro">Solteiro</SelectItem>
-                    <SelectItem value="casado">Casado</SelectItem>
-                    <SelectItem value="divorciado">Divorciado</SelectItem>
-                </SelectContent>
-            </Select>
-          <Input placeholder="(xx) xxxxx-xxxx" required />
-          <Input placeholder="exemplo@gmail.com" required />
-          <Input placeholder="CEP" required />
-          <Input placeholder="Endereço" required />
-          <Input placeholder="Cidade" required />
-          <Input placeholder="Bairro" required />
-          <Input placeholder="UF" required />
-          <Input placeholder="Naturalidade" required />
-          <Input placeholder="Conhecido por..." required />
-          <Select>
-            <SelectTrigger className="w-full">
-                <SelectValue placeholder="Escolaridade" />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectItem value="fundamental">Fundamental</SelectItem>
-                <SelectItem value="medio">Médio</SelectItem>
-                <SelectItem value="superior">Superior</SelectItem>
-            </SelectContent>
-        </Select>
-          <Input placeholder="(xx) xxxxx-xxxx" required />
-          <Input placeholder="Onde trabalha?" required />
+          <FormInput label="Nome:" placeholder="Digite seu nome completo" required />
+          <FormSelect label="Sexo:" placeholder="Sexo" required options={[
+              { label: "Masculino", value: "1" },
+              { label: "Feminino", value: "2" }
+          ]} />
+          <FormInput label="Data de Nascimento:" placeholder="" type="date" required />
+          <FormSelect label="Estado Civil:" placeholder="Estado Civil" required options={[
+              { label: "Solteiro", value: "1" },
+              { label: "Casado", value: "2" },
+              { label: "Divorcido", value: "3"}
+          ]} />
+          <FormInput label="Telefone:" placeholder="(xx) xxxxx-xxxx" required />
+          <FormInput label="Email:" placeholder="Email" type="email" required />
+          <FormInput label="CEP" placeholder="xxxxx-xxx" required />
+          <FormInput label="Endereço:" placeholder="Endereço" required />
+          <FormInput label="Cidade:" placeholder="Cidade" required />
+          <FormInput label="Complemento:" placeholder="Complemento" />
+          <FormInput label="Bairro:" placeholder="Bairro" required />
+          <FormInput label="UF:" placeholder="UF" disabled/>
+          <FormInput label="Naturalidade:" placeholder="Naturalidade" />
+          <FormInput label="Conhecido por:" placeholder="Apelido" />
+          <FormSelect label="Escolaridade:" placeholder="Escolaridade" options={[
+              { label: "Fundamental", value: "1" },
+              { label: "Médio", value: "2" },
+              { label: "Superior", value: "3"}
+          ]} />
+          <FormInput label="Telefone do trabalho:" placeholder="(xx) xxxxx-xxxx" />
+          <FormInput label="Local de trabalho:" placeholder="Empresa" />
         </div>
 
-        <h3 className="mb-2 font-semibold">DADOS FAMILIARES</h3>
+        <h3 className="mb-2 font-bold">DADOS FAMILIARES</h3>
         <div className={inputstyle}>
-          <Input placeholder="Nome do cônjuge" />
-          <Input placeholder="(xx) xxxxx-xxxx" />
-          <Input type="date" placeholder="Data do casamento" />
-          <Input placeholder="Nome do pai" />
-          <Input placeholder="Nome da mãe" />
-          <Input type="number" maxLength={1} placeholder="N° de Filhos" />
+          <FormInput label="Nome do Cônjuge:" placeholder="Nome do Cônjuge" type="text" disabled/>
+          <FormInput label="Telefone do Cônjuge:" placeholder="(xx) xxxxx-xxxx" disabled />
+          <FormInput label="Data do Casamento:" placeholder="" type="date" disabled />
+          <FormInput label="Nome do Pai:" placeholder="Nome do Pai" type="text" />
+          <FormInput label="Nome da Mãe:" placeholder="Nome da Mãe" type="text" />
+          <FormInput label="Nº de Filhos:" placeholder="Nº de filhos" type="number" maxLength={1}/>
         </div>
 
-        <h3 className="mb-2 font-semibold">DADOS MINISTERIAIS</h3>
+        <h3 className="mb-2 font-bold">DADOS MINISTERIAIS</h3>
         <div className={inputstyle}>
-          <Input placeholder="Ministério que faz parte" />
-          <Input placeholder="Função que exerce" />
-          <div className="flex justify-start items-center mb-2">
-              <Checkbox className="m-0 mr-2 w-6 h-6"></Checkbox><p>Lider de GFCD?</p>
-          </div>
-          <Input type="date" placeholder="Data da conversão" />
-          <div className="flex justify-start items-center mb-2">
-              <Checkbox className="m-0 mr-2 w-6 h-6"></Checkbox><p>Batizado?</p>
-          </div>
-          <Input placeholder="Igreja do batismo" />
-          <Input type="date" placeholder="Data do batismo" />
-          <Select>
-            <SelectTrigger className="w-full">
-                <SelectValue placeholder="Tipo de admissão" />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectItem value="fundamental">Batismo</SelectItem>
-                <SelectItem value="medio">Aclamação</SelectItem>
-                <SelectItem value="superior">Transferência</SelectItem>
-            </SelectContent>
-        </Select>
-          <Input type="date" placeholder="Data da admissão" />
+          <FormInput label="Ministério que faz parte:" placeholder="Ministério que faz parte" type="text" />
+          <FormInput label="Função que exerce" placeholder="Função que exerce" type="text" />
+          <FormCheckbox id="gfcdLider" label="Lider de GFCD?" />
+          <FormInput label="Data da Conversão:" placeholder="" type="date" required/>
+          <FormCheckbox id="seBatizado" label="Batizado?" />
+          <FormInput label="Igreja do Batismo" placeholder="Igreja do Batismo" type="text" disabled />
+          <FormInput label="Data do Batismo" placeholder="" type="date" disabled/>
+          <FormSelect label="Tipo de Admissão:" placeholder="Tipo de Admissão" options={[
+              { label: "Batismo", value: "1" },
+              { label: "Aclamação", value: "2" },
+              { label: "Transferência", value: "3"}
+          ]} />
+          <FormInput label="Data da Admissão:" placeholder="" type="date" disabled/>
         </div>
 
-        <h3 className="mb-2 font-semibold">DADOS ADICIONAIS</h3>
+        <h3 className="mb-2 font-bold">DADOS ADICIONAIS</h3>
         <div className={inputstyle}>
-            <Select>
-            <SelectTrigger className="w-full">
-                <SelectValue placeholder="GFCD que frequenta?" />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectItem value="1">REUEL</SelectItem>
-                <SelectItem value="2">DEUS É FIEL</SelectItem>
-            </SelectContent>
-        </Select>
-        <div className="flex justify-start items-center mb-2">
-              <Checkbox className="m-0 mr-2 w-6 h-6"></Checkbox><p>Já foi consolidado?</p>
-        </div>
-        <Input placeholder="Nome do consolidador" />
-        <Select>
-            <SelectTrigger className="w-full">
-                <SelectValue placeholder="Já fez Retiro?" />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectItem value="1">Pre-encontro</SelectItem>
-                <SelectItem value="2">Encontro</SelectItem>
-                <SelectItem value="3">Pós-encontro</SelectItem>
-            </SelectContent>
-        </Select>
+          <FormSelect label="Escolaridade:" placeholder="Escolaridade" options={[
+              {label: "Fundamental", value: "1" },
+              {label: "Médio", value: "2" },
+              {label: "Superior", value: "3"}
+          ]} />
+          <FormSelect label="GFCD que frequenta:" placeholder="GFCD que frequenta" options={[ 
+              { label: "CELULA", value: "1" },
+              { label: "CELULA", value: "2" },
+              { label: "CELULA", value: "3" },
+              { label: "CELULA", value: "4" },
+              { label: "CELULA", value: "5" },
+              { label: "CELULA", value: "6" },              
+          ]} />
+          <FormCheckbox id="gfcdConsolidado" label="Já foi consolidado?" />
+          <FormInput label="Nome do Consolidador:" placeholder="Nome do Consolidador" type="text"/>
+          <FormSelect label="Já fez retiro?" placeholder="Já fez retiro?" options={[
+              { label: "Pre-encontro", value: "1" },
+              { label: "Encontro", value: "2"},
+              { label: "Pós-encontro", value: "3"}
+          ]} />
         </div>
 
         <div className="flex justify-center">
-          <Button className="w-full h-10" type="submit">REGISTRAR</Button>
+          <Button id="btn-register" className="w-full" type="submit">REGISTRAR</Button>
         </div>
       </form>
     </section>
