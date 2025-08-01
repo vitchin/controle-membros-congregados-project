74e81d76afcc1c7093efd3430c0345e81bb9176a
Victor Ricardo Oliveira Nunes <victor12092004@gmail.com>
2025-08-01 14:50:27 -0300

    [feat] login.txs | implementa componente reutilizavel

--- iebi-membership-app/src/app/components/login.tsx
+++ iebi-membership-app/src/app/components/login.tsx
@@ -1,25 +1,18 @@
-import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-
-const inputstyle = "form-group mb-6 w-full flex flex-col align-start justify-center";
+import { FormInput } from "./componentInput";
 
 export function Login() {
   return (
     <section className="w-100 container mx-auto my-10 px-5 py-8 bg-white rounded-lg shadow-md">
       <h2 className="text-center text-2xl font-bold mb-2">LOGIN</h2>
       <p className="mb-4 text-center text-gray-500">Informe seus dados de login para acessar.</p>
-      <form>
-        <div className={inputstyle}>
-          <Label>Usuário:</Label>
-          <Input type="text" id="username" name="username" required />
-        </div>
-        <div className={inputstyle}>
-          <Label>Senha:</Label>
-          <Input type="password" id="password" name="password" required />
-        </div>
-        <div className="flex justify-center">
-          <Button className="w-full h-10" type="submit">ENTRAR</Button>
+      <form className="flex flex-col gap-6">
+
+        <FormInput id="userLogin" label="Usuário:" placeholder="Digite o nome de usuário" type="text" required/>
+        <FormInput id="senhaLogin" label="Senha:" placeholder="Digite sua senha" type="password" required/>
+        
+        <div className="m-0 p-0 flex justify-center">
+          <Button id="btn-login" className="w-full" type="submit">ENTRAR</Button>
         </div>
       </form>
     </section>
